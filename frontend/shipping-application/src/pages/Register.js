43f5dba7af6f28f3e718905f8ae5d96f35750e4e
Karam Al Ashkar <karam.alachkar16@gmail.com>
2023-02-14 13:27:09 +0200

    Frontend <> Adding register function in register page

--- frontend/shipping-application/src/pages/Register.js
+++ frontend/shipping-application/src/pages/Register.js
@@ -1,18 +1,50 @@
 import "./style.css";
 import Input from '../components/Input';
 import FormButton from '../components/FormButton';
+import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { passwordValidation } from '../utilities/Validator';
+import { register } from "../api/register";
 
 const Register =()=>{
+  const navigate=useNavigate()
+  const [name,setName]=useState('')
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+  const [errorMessage,setErrorMessage]=useState('')
+
+  const signUp = async(e) =>{
+      e.preventDefault()
+      if(!name || !email || !password){
+          setErrorMessage('Enter all required fields')
+          return null
+      }
+
+      const matchPassword=passwordValidation(password);
+
+      if(matchPassword){
+          const post={name,email,password}
+          const response=await register(post)
+          if(response=="success"){
+              navigate("/login")
+          }else{
+              setErrorMessage('Email already exist')
+          }
+      }else{
+          setErrorMessage('Enter a strong password')
+      }
+  }
+      
   return (
     <div className='form'>
       <div className='user-form'>
         <img src={'logo.png'} />
         <h1>REGISTER</h1>
-        <p style={{color: 'red'}}></p>
-        <form>
-          <Input type="text" text="Name" />
-          <Input type="email" text="Email" />
-          <Input type="password" text="Password" />
+        <p style={{color: 'red'}}>{errorMessage}</p>
+        <form onSubmit={signUp}>
+          <Input type="text" text="Name" setValue={setName} setError={setErrorMessage} />
+          <Input type="email" text="Email" setValue={setEmail} setError={setErrorMessage} />
+          <Input type="password" text="Password" setValue={setPassword} setError={setErrorMessage} />
           <FormButton text="REGISTER" />
         </form>
       </div>
