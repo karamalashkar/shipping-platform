1f7c3333236d0929cf89bebf04b8cb619aec1cf0
Karam Al Ashkar <karam.alachkar16@gmail.com>
2023-02-14 15:59:30 +0200

    Frontend <> Adding add shipment component to home page

--- frontend/shipping-application/src/pages/Home.js
+++ frontend/shipping-application/src/pages/Home.js
@@ -4,8 +4,10 @@ import Navbar from "../components/Navbar";
 import ShipmentCard from "../components/ShipmentCard";
 import Button from "../components/Button";
 import { getShipment } from "../api/getShipment";
+import AddShipment from "../components/AddShipment";
 
 const Home=()=>{
+    const [isOpenAdd,setOpenAdd]=useState(false)
     const [data,setData]=useState('')
     const userID=localStorage.getItem('id');
 
@@ -19,25 +21,29 @@ const Home=()=>{
     },[])
 
     return(
-        <div className="home">
-            <Navbar />
-            <div className="add-section">
-                <Button text="Add Shipment" color={'#34495e'} />
+        <>
+            <div className="home">
+                <Navbar />
+                <div className="add-section">
+                    <Button text="Add Shipment" color={'#34495e'} onClick={()=>setOpenAdd(true)} />
+                </div>
+                <div className="shipments">
+                    {
+                        Object.values(data).map( (data) => {
+                            return(
+                                <ShipmentCard key={data.id}
+                                image={data.waybill}
+                                name={data.customer_name}
+                                address={data.customer_address}
+                                phone={data.customer_phone_number} />
+                            );
+                        })
+                    }
+                </div>
             </div>
-            <div className="shipments">
-                {
-                    Object.values(data).map( (data) => {
-                        return(
-                            <ShipmentCard key={data.id}
-                            image={data.waybill}
-                            name={data.customer_name}
-                            address={data.customer_address}
-                            phone={data.customer_phone_number} />
-                        );
-                    })
-                }
-            </div>
-        </div>
+
+            <AddShipment open={isOpenAdd} onClose={()=>setOpenAdd(false)} />
+        </>
     );
 }
 
