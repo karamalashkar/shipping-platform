111a3ad6cf6efbcb6debc7fcfb9436bbf135631a
Karam Al Ashkar <karam.alachkar16@gmail.com>
2023-02-14 02:31:55 +0200

    Backend <> Adding add new shipment function

---
+++ backend/shipping-server/app/Http/Controllers/ShipmentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Shipment;
+
+use Illuminate\Http\Request;
+
+class ShipmentController extends Controller{
+
+    function addShipment(Request $request){
+        $waybill=$request->waybill;
+        $userID=$request->userID;
+        
+        $date=date("Y-m-d");
+        $time=date("h-i-s");
+        $shipmentNumber=$userID.$date.$time;
+
+        $shipment=new Shipment;
+
+        $shipment->customer_name=$request->customerName;
+        $shipment->customer_address=$request->customerAddress;
+        $shipment->customer_phone_number=$request->customerPhoneNumber;
+        $shipment->user_id=$userID;
+
+        $base64Image = explode(";base64,", $waybill);
+        $explodeImage = explode("image/", $base64Image[0]);
+        $imageType = $explodeImage[1];
+        $image_base64 = base64_decode($base64Image[1]);
+        $imageName = $shipmentNumber . '.'.'png';
+        \File::put(public_path('assets'). '/' . $imageName, base64_decode($base64Image[1]));
+        $shipment->waybill=$imageName;
+
+        $shipment->save();
+        return response()->json([
+            'status' => 'success'
+        ]);
+    }
+}
