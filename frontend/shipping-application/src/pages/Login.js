ef568c086f3fd0d71de70d26188e1be07e7edab4
Karam Al Ashkar <karam.alachkar16@gmail.com>
2023-02-14 13:00:55 +0200

    Frontend <> Adding login function in login page

--- frontend/shipping-application/src/pages/Login.js
+++ frontend/shipping-application/src/pages/Login.js
@@ -1,17 +1,44 @@
 import "./style.css";
+import { useState } from "react";
 import Input from "../components/Input";
 import FormButton from "../components/FormButton";
+import { login } from "../api/login";
+import { useNavigate } from 'react-router-dom';
 
 const Login=()=>{
+  const navigate=useNavigate()
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+  const [errorMessage, setErrorMessage]=useState('')
+
+  const signIn = async(e) =>{
+      e.preventDefault()
+      if(!email || !password){
+          setErrorMessage('Enter all required fields')
+          return null
+      }
+
+      const post={email,password}
+      const response=await login(post)
+      if(response.status == 'success'){
+          localStorage.setItem('id',response.user.id)
+          localStorage.setItem('token',response.token)
+          navigate("/home")
+      }
+      else{
+          setErrorMessage('Invalid Credential')
+      }
+  }
+
   return (
     <div className='form'>
       <div className='user-form'>
         <img src={'logo.png'} />
-        <h1>Login</h1>
-        <p style={{color: 'red'}}></p>
-        <form>
-          <Input type="email" text="Email" />
-          <Input type="password" text="Password" />
+        <h1>LOGIN</h1>
+        <p style={{color: 'red'}}>{errorMessage}</p>
+        <form onSubmit={signIn}>
+          <Input type="email" text="Email" setValue={setEmail} setError={setErrorMessage} />
+          <Input type="password" text="Password" setValue={setPassword} setError={setErrorMessage} />
           <FormButton text="LOGIN" />
           <p>Don't have an account? <a href="/register">Join Now</a></p>
         </form>
