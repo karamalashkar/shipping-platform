93f42e16a941e459620fb420f25f6a029c814f7d
Karam Al Ashkar <karam.alachkar16@gmail.com>
2023-02-14 11:30:08 +0200

    Frontend <> Adding landing page

---
+++ frontend/shipping-application/src/pages/Landing.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const Landing = () =>{
+    return(
+        <div className="landing">
+            <div className="hero-text">
+                <h1>Shipping</h1>
+                <h2>Start Your Own Shipment</h2>
+                <button><a href="/login">Join Now</a></button>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
