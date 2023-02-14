1369c8b235685f738a64a1b367cbf60e04ccb86a
Karam Al Ashkar <karam.alachkar16@gmail.com>
2023-02-14 02:40:54 +0200

    Backend <> Adding all needed routes

--- backend/shipping-server/routes/api.php
+++ backend/shipping-server/routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ShipmentController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => "auth:api"],function(){
+    Route::post('/shipment',[ShipmentController::class,'addShipment']);
+    Route::get('/shipments/{id}',[ShipmentController::class,'getShipments']);
+    Route::get('/shipment/{id}',[ShipmentController::class,'getShipmentById']);
+    Route::post('/shipment/{id}',[ShipmentController::class,'updateShipment']);
+    Route::delete('/shipment/{id}',[ShipmentController::class,'deleteShipment']);
 });
+
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
