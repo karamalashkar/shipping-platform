aa6a6f5da7547d3393db9b3dba8dba153787ff58
Karam Al Ashkar <karam.alachkar16@gmail.com>
2023-02-14 15:06:14 +0200

    Frontend <> Adding get shipments function

--- frontend/shipping-application/src/pages/Home.js
+++ frontend/shipping-application/src/pages/Home.js
@@ -1,9 +1,23 @@
 import "./style.css";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import ShipmentCard from "../components/ShipmentCard";
 import Button from "../components/Button";
+import { getShipment } from "../api/getShipment";
 
 const Home=()=>{
+    const [data,setData]=useState('')
+    const userID=localStorage.getItem('id');
+
+    const shipment = async ()=>{
+        const result=await getShipment(userID);
+        setData(result)
+    }
+
+    useEffect(()=>{
+        shipment()
+    },[])
+
     return(
         <div className="home">
             <Navbar />
@@ -11,7 +25,17 @@ const Home=()=>{
                 <Button text="Add Shipment" color={'#34495e'} />
             </div>
             <div className="shipments">
-                <ShipmentCard />
+                {
+                    Object.values(data).map( (data) => {
+                        return(
+                            <ShipmentCard key={data.id}
+                            image={data.waybill}
+                            name={data.customer_name}
+                            address={data.customer_address}
+                            phone={data.customer_phone_number} />
+                        );
+                    })
+                }
             </div>
         </div>
     );
